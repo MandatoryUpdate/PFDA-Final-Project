d16af4b0d2d24598d7bf46e03676c0559c88b9d1
Johan Park <johan.park97@gmail.com>
2025-04-25 17:07:05 -0500

    Basic Player Movement

--- src/project.py
+++ src/project.py
@@ -4,14 +4,49 @@ from pygame import mixer
 
 
 class Player():
-    def __init__(self, isFalling = True, keyPressed = "", isTouching =""):
+    def __init__(self, pos = (0,0), isFalling = True, keyPressed = "", isTouching = False):
         self.isFalling = isFalling
         self.keyPressed = keyPressed
         self.isTouching = isTouching
+        self.gravityVal = 0
+        self.pos = pos
+        self.surface = pygame.Surface(size=(10, 10))
+        pygame.draw.rect(surface = self.surface, color=(255, 255, 255), rect=(0,0, 10, 10))
 
+    def setFalling(self, val):
+        self.isFalling = val
+    
+    def setTouching (self, val):
+        self.isTouching = val
+    
+    def adjustYPosition(self):
+        x, y = self.pos
+        y += self.gravityVal
+        self.pos = (x, y)
+    
+    def adjustHorizontalPosition(self, keyPressed):
+        x, y = self.pos
+        if keyPressed == "left":
+            x -= 1
+            self.pos = (x, y)
+        if keyPressed == "right":
+            x += 1
+            self.pos = (x, y)
+            
+    
+    def draw(self, surface):
+        surface.blit(self.surface, self.pos)
 
-        def setFalling(self, val):
-            self.isFalling = val
+    def update(self, surface, keyPressed):
+        if self.isFalling:
+            if self.gravityVal < 2:
+                self.gravityVal += .005
+        self.adjustYPosition()
+        self.adjustHorizontalPosition(keyPressed)
+        self.draw(surface)
+        
+
+        
 
 
 
@@ -25,12 +60,12 @@ def main():
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
     running = True
+    player = Player((400, 150), True, "", False)
     fullscreen = False
     resolution2 = resolution
+    keyPressed = ""
 
     while running:
-        mouse_position = pygame.mouse.get_pos()
-        #Event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -50,6 +85,14 @@ def main():
                     pygame.display.quit()
                     pygame.display.init()
                     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                keyPressed = "left"
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                keyPressed = "right"
+
+        screen.fill((0, 0, 0))
+        player.update(screen, keyPressed)
+        pygame.display.flip()
 
 
 
