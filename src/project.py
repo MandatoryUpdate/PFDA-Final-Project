cd1379b43201856434dff7d593461144303173cf
Johan Park <johan.park97@gmail.com>
2025-04-28 20:36:57 -0500

    Attempt at adding platforms.

--- src/project.py
+++ src/project.py
@@ -2,45 +2,84 @@ import random
 import pygame
 from pygame import mixer
 
+class jumpingObject():
+    def __init__(self, screenSize = (0,0), pos = (0,0), width = 800, height = 5):
+        self.screenSize = screenSize
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.surface = pygame.Surface(size=(width, height))
+        self.hitBox = pygame.Rect((0, 0), (width, height))
+    
+    def draw(self, surface):
+        surface.blit(self.surface, self.pos)
+        pygame.draw.rect(surface=self.surface, color=(255, 0, 0), rect=self.hitBox)
+    
+    def returnHitBox(self):
+        return self.hitBox
+    
+    def update(self, surface):
+        self.draw(surface)
 
 class Player():
-    def __init__(self, pos = (0,0), isFalling = True, keyPressed = "", isTouching = False):
+    def __init__(self, pos = (0,0), isFalling = True, keyPressed = ""):
         self.isFalling = isFalling
         self.keyPressed = keyPressed
-        self.isTouching = isTouching
         self.gravityVal = 0
         self.pos = pos
         self.surface = pygame.Surface(size=(10, 10))
-        pygame.draw.rect(surface = self.surface, color=(255, 255, 255), rect=(0,0, 10, 10))
+        self.hitBox = pygame.Rect((0, 0), (10, 10))
+        pygame.draw.rect(surface = self.surface, color=(255, 0, 0), rect=self.hitBox)
 
     def setFalling(self, val):
         self.isFalling = val
+
+    def getPos(self):
+        return self.pos
     
-    def setTouching (self, val):
+    def checkTouching (self, val):
         self.isTouching = val
+        if self.isTouching:
+            self.setFalling(False)
+        else:
+            self.setFalling(True)
+    
+    def returnHitBox(self):
+        return self.hitBox
+    
+    def returnPosition(self):
+        return self.pos
     
     def adjustYPosition(self):
         x, y = self.pos
         y += self.gravityVal
         self.pos = (x, y)
+        self.hitBox = pygame.Rect((x, y), (10, 10))
     
     def adjustHorizontalPosition(self, keyPressed):
         x, y = self.pos
         if keyPressed == "left":
             x -= 1
             self.pos = (x, y)
+            self.hitBox = pygame.Rect((x, y), (10, 10))
         if keyPressed == "right":
             x += 1
             self.pos = (x, y)
-            
+            self.hitBox = pygame.Rect((x, y), (10, 10))
+    
+
     
     def draw(self, surface):
+        pygame.draw.rect(surface = self.surface, color=(255, 0, 0), rect=self.hitBox)
         surface.blit(self.surface, self.pos)
 
     def update(self, surface, keyPressed):
         if self.isFalling:
             if self.gravityVal < 2:
                 self.gravityVal += .005
+        elif not self.isFalling:
+            self.gravityVal = -3
+            self.setFalling(True)
         self.adjustYPosition()
         self.adjustHorizontalPosition(keyPressed)
         self.draw(surface)
@@ -60,7 +99,8 @@ def main():
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
     running = True
-    player = Player((400, 150), True, "", False)
+    player = Player((400, 200), True, "")
+    testObject = jumpingObject(resolution, (0, 500), 800, 5)
     fullscreen = False
     resolution2 = resolution
     keyPressed = ""
@@ -89,9 +129,12 @@ def main():
                 keyPressed = "left"
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
                 keyPressed = "right"
-
+            if player.returnHitBox().colliderect(testObject.returnHitBox()):
+                player.setFalling(False)
         screen.fill((0, 0, 0))
         player.update(screen, keyPressed)
+        testObject.update(screen)
+
         pygame.display.flip()
 
 
